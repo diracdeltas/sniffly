19f9cf8131f545750d3ca6409edb37f0a70b5d63
yan <yzhu@yahoo-inc.com>
2015-10-20 16:09:00 -0700

    Speedup synchronous loads
    Timeout loads after 20ms since that is the confirmation cutoff.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ var TIMING_UPPER_THRESHOLD = 4;
 var TIMING_LOWER_THRESHOLD = -10;
 // Timing allowance for a synchronous image load, which we use to confirm
 // positive results in Chrome.
-var TIMING_CONFIRM_THRESHOLD = 10;
+var TIMING_CONFIRM_THRESHOLD = 20;
 
 // Use an arbitrary static preloaded HSTS host for timing calibration
 var BENCHMARK_HOST = 'http://torproject.org/';
@@ -23,6 +23,7 @@ var OFFSET = 0;
 var visitedElem = document.getElementById('visited');
 var notVisitedElem = document.getElementById('not_visited');
 var isFirefox = (window.navigator.userAgent.indexOf('Firefox') !== -1);
+var visited = []; // list of hosts that are potentially visited
 
 // Edit this based on scraper results.
 var hosts =
@@ -358,6 +359,13 @@ var hosts =
 'http://www.cloudflare.com/'];
 
 /**
+ * Gets hostname from URL.
+ */
+function getHost_(url) {
+  return url.replace('http://', '').split(/\/|\?/)[0];
+}
+
+/**
  * Our CSP policy (HTTP-only images) causes this to fire whenever the img src
  * redirects to HTTPS, either by HSTS (307) or plain old redirects (301/302).
  * @param {number} start Time when the image load started
@@ -388,31 +396,58 @@ function onImgError_(start, host) {
  * @private
  */
 function confirmVisited_(callback, finished) {
-  var initial = 0;
+  var initial; // initial time
   var img = new Image();
+  var timeouts = []; // array of timeout IDs
+  var hostsDone = [];
+  var dummySrc = 'http://example.com/'; // URL for timer initialization
+  function clearTimeouts_() {
+    // Clear existing timeouts
+    timeouts.forEach(function(id) {
+      window.clearTimeout(id);
+    });
+    timeouts = [];
+  }
   function doNext_() {
     if (visited.length === 0) {
       finished();
       return;
     }
-    // Shift instead of pop since we may still be adding hosts to the array
-    // while this is running.
+    // Shift instead of pop since we are pushing hosts into the array while
+    // this is running
     var host = visited.shift();
     initial = new Date().getTime();
-    img.src = 'http://' + host + '?' + initial.toString();
+    var src = 'http://' + host + '/?' + initial.toString();
+    img.src = src;
+    // Abort after 20ms since positive results should take less time anyway
+    timeouts.push(window.setTimeout(img.onerror.bind({src: src}),
+                                    TIMING_CONFIRM_THRESHOLD));
   }
   img.onerror = function() {
-    var time = new Date().getTime();
-    callback(this.src, time - initial);
+    if (this.src !== dummySrc) {
+      clearTimeouts_();
+      var host = getHost_(this.src);
+      if (hostsDone.indexOf(host) !== -1) {
+        // We might have called the callback for this host already.
+        console.log('already done, skipping', host);
+      } else {
+        hostsDone.push(host);
+        callback(host, new Date().getTime() - initial);
+      }
+    } else {
+      console.log('initialized timer using', this.src);
+    }
     doNext_();
-  }
+  };
   img.onload = function() {
     // Should never happen but add a callback in case so it doesn't block the
     // rest of the image requests from being sent.
-    console.log('LOADED', this.src);
+    console.log('UNEXPECTEDLY LOADED', this.src);
     doNext_();
-  }
-  doNext_();
+  };
+  // Set the image source initially to a dummy URL b/c the first load seems to
+  // always take a long time no matter what.
+  img.src = 'http://example.com/';
 }
 
 /**
@@ -446,14 +481,12 @@ function calibrateTime() {
  * @param {number} time
  * @param {number} offset
  */
-var visited = []; // list of hosts that are potentially visited
 function display(url, time, offset) {
   var li = document.createElement('li');
-  var host = url.replace('http://', '').split('/')[0];
+  var host = getHost_(url);
   li.id = host;
   li.appendChild(document.createTextNode(host));
   if (time < TIMING_UPPER_THRESHOLD && time > TIMING_LOWER_THRESHOLD) {
-    console.log(host, time, offset);
     if (!isFirefox) {
       // If we are in Chrome, hide the results for now because the false
       // positive rate is really high until confirmVisited_() is called.
@@ -473,18 +506,22 @@ if (!isFirefox) {
   var disclaimer = document.getElementById('disclaimer');
   disclaimer.style.display = '';
   window.setTimeout(function() {
-    confirmVisited_(function(src, t) {
+    confirmVisited_(function(host, t) {
       if (!disclaimer.done_) {
         disclaimer.style.color = 'orange';
         disclaimer.innerText = 'Removing false positives . . .';
         disclaimer.done_ = true;
       }
-      var host = src.replace('http://', '').split('/')[0];
       var elem = document.getElementById(host);
+      if (!elem) {
+        console.warn('No element found', host);
+        return;
+      }
       if (t < TIMING_CONFIRM_THRESHOLD) {
-        console.log('confirmed', src, t);
+        console.log('showing', host, t);
         elem.style.color = '';
       } else {
+        console.log('hiding', host, t);
         elem.style.display = 'none';
         notVisitedElem.appendChild(elem);
       }
